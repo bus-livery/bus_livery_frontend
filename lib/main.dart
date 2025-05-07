9250e4fe1d47afde704e959de9e95e0acb83a498
Jithin Johnson <92211733+centrric-cntc0020@users.noreply.github.com>
2025-05-07 16:35:20 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bot_toast/bot_toast.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -29,6 +30,8 @@ void main() async {
   getIt<UpdateService>().checkForUpdatesInBackground();
 
   await ScreenUtil.ensureScreenSize();
+
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
