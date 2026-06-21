0d76d3ee3c92608e7d17f2356ebec046ee883364
Jithin Johnson <iamjithinjohnson@gmail.com>
2026-06-21 16:24:24 +0530

    chore: bump version to 2.6.0 and switch to production API and AdMob units

--- lib/service/dio_service.dart
+++ lib/service/dio_service.dart
@@ -19,8 +19,8 @@ class DioService {
   final Dio dio;
 
   SharedPrefService sharedPS = getIt<SharedPrefService>();
-  // static const baseUrl = "http://18.212.92.48/api";
-  static const baseUrl = "http://192.168.17.74:8080/api";
+  static const baseUrl = "http://18.212.92.48/api";
+  // static const baseUrl = "http://192.168.17.74:8080/api";
   // static const baseUrl = "http://localhost:8080/api";
 
   DioService() : dio = Dio(BaseOptions(baseUrl: baseUrl)) {
